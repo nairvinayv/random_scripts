cb228fb532fe5a864e363da6ed60d36f5e546469
Vinay Nair <nairvinayv@users.noreply.github.com>
2015-08-26 19:07:30 +0530

    Delete polar_plot.r

--- polar_plot.r
+++
@@ -1,8 +0,0 @@
-#R-Code for generating Polar plots using the plotrix library
-library(plotrix)
-data<-read.csv('15T-epsilon.dat')
-
-setEPS()
-png("15T-epsilon.png")
-polar.plot(data[1:14999,1],data[1:14999,2],rp.type="p",start=90,clockwise=TRUE,main=expression(paste("Torsion Angles: 1,5T-",epsilon)))
-dev.off()
