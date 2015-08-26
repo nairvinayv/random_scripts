d7f294ee0c088f1f066c7d3585e0cd7e3dc800e0
Vinay Nair <nairvinayv@users.noreply.github.com>
2015-08-26 11:40:46 +0530

    Create dialplot.r

---
+++ dialplot.r
@@ -0,0 +1,7 @@
+library(plotrix)
+data<-read.csv('15T-epsilon.dat')
+
+setEPS()
+png("15T-epsilon.png")
+polar.plot(data[1:14999,1],data[1:14999,2],rp.type="p",start=90,clockwise=TRUE,main=expression(paste("Torsion Angles: 1,5T-",epsilon)))
+dev.off()
